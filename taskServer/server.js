25ce9ebf2e17a1285039f5c4f43c13b0411fae2a
r0m4 <formicrotech@gmail.com>
2014-11-10 21:53:41 +0200

    отступы по серьезному поправил!

--- taskServer/server.js
+++ taskServer/server.js
@@ -1,17 +1,16 @@
 var http = require('http');
 var fs = require('fs');
 
-
 console.log("starting");
 
-http.createServer(function (req, res) {
-    
-    if (req.url === "/test") {
+    http.createServer(function (req, res) {
+        
+        if (req.url === "/test") {
             fs.readFile('text.txt', 'utf8', function (err, content) {
                 if (err) {
                     res.statusCode = 500;
                     res.end = ("Server Error");
-                
+                    
                 } else {
                     res.setHeader("Content-Type", "text/plain; charset=utf-8");
                     console.log(content);
@@ -19,28 +18,28 @@ http.createServer(function (req, res) {
                     res.end(content);
                 }
             });
-    }
+        }
 
-    if (req.url === "/index") {
+        if (req.url === "/index") {
             fs.readFile('index.html', 'utf8', function (err, content) {
                 if (err) {
                     res.statusCode = 500;
                     res.end = ("Server Error");
-                
+                    
                 } else {
                     res.setHeader("Content-Type", "text/html; charset=utf-8");
                     console.log(content);
                     res.end(content);
                 }
             });
-    }
+        }
 
-    if (req.url === "/picture") {
-           fs.readFile('Dinka.png', function (err, content) {
+        if (req.url === "/picture") {
+            fs.readFile('Dinka.png', function (err, content) {
                 if (err) {
                     res.statusCode = 500;
                     res.end = ("Server Error");
-                
+                    
                 } else {
                     res.setHeader("Content-Type", "image/png");
                     console.log(content);
@@ -48,14 +47,14 @@ http.createServer(function (req, res) {
                 }
             }); 
 
-    } 
+        } 
 
-    if (req.url === '/') {          
-           res.writeHead(200, {"Content-type":"text/plain; charset=utf-8"});
-           res.end("Hello World its meeeee");
+        if (req.url === '/') {          
+            res.writeHead(200, {"Content-type":"text/plain; charset=utf-8"});
+            res.end("Hello World its meeeee");
 
-    }
+        }
 
-    console.log("Received request: " + req.url);
+        console.log("Received request: " + req.url);
 
 }).listen(3000, function () {console.log("listening on port 3000")});
