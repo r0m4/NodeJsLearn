ea6939d4b4bc67ad193bdff8e3a8abe2a4a8248e
r0m4 <formicrotech@gmail.com>
2014-11-17 23:49:07 +0200

    дошел до ошибки не знаю чё делать

---
+++ GivingFiles/GetAll.js
@@ -0,0 +1,71 @@
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+var name;
+var options = 
+    [
+    {
+        host: "localhost",
+        port: 3000,
+        path: '/index'
+    }, 
+    
+    {
+        host: "localhost",
+        port: 3000,
+        path: '/test'
+    },
+    
+    {
+        host: "localhost",
+        port: 3000,
+        path: '/picture'
+    }
+
+    ];
+    
+    
+    
+    for (var i = 0; i < options.length; i++) {
+        //console.log(options[i]);
+        server(options[i]);    
+    }
+
+    
+
+    function server (path) {
+        
+
+        var data = http.get(options, function(res) {
+            console.log("Got response: " + res.statusCode);
+            var lastDig = (res.headers['content-type'].indexOf(';'));
+            
+            if(res.headers['content-type'] === "image/png") {
+                name = ".png";
+
+            }
+            else if(res.headers['content-type'].slice(0, lastDig) === "text/plain") {
+                name = ".txt";
+
+            }
+            else if (res.headers['content-type'].slice(0, lastDig) === "text/html") {
+                name = ".html";
+
+            } else name = ".bin";
+
+            res.on("data", function(chunk) {
+                
+                fs.writeFile(path.slice(1)+name,""+chunk, function(err) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        console.log("The file "+path.slice(1)+" was saved!")
+                    }
+                });
+            })
+        }).on('error', function(e) {
+            console.log("Got error: "+e.message);
+        });
+
+    };
+   
